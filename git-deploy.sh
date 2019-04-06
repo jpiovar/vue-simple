abd2aa11b52d7a755d6c73076bab3b6787547a12
Juraj Piovar <jpiovar@sk.ibm.com>
2019-04-06 21:51:07 +0200

    small change

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 while true; do
-    git pull origin master
+    git pull origin master && [
+        echo "deployment script triggered base on change => repo merged or updated"
+    ]
     sleep 120
 done
