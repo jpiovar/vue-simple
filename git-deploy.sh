8aeb5058c8555e2911f0bea5ad8ff92cb0883ee7
Juraj Piovar <jpiovar@sk.ibm.com>
2019-04-06 22:20:26 +0200

    git-bash file modified

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 while true; do
-    git pull origin master && [
-        echo "deployment script triggered base on change => repo merged or updated"
-    ]
+    git pull origin master && echo "deployment script triggered base on change => repo merged or updated"
     sleep 120
 done
