ede41758d653b4f4423235411b1a5d7987811ebb
Juraj Piovar <jpiovar@sk.ibm.com>
2019-04-10 20:22:44 +0200

    bash updated added package with build command

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+command -v nvm || {
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+    source $HOME/.nvm/nvm.sh
+}
+command -v node || {
+    nvm install 11.9
+    nvm use 11.9
+}
+
 while true; do
     git pull origin master && {
         echo "deployment script triggered base on change => repo merged or updated"
-        
+        npm run build
     }
     sleep 120
 done
